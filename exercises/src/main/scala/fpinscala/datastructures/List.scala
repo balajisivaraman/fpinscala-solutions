a349d717587b2b1e11c49e4e59408b09bef55666
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-04-19 01:27:57 +0530

    Complete implementations for Chapter 3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,19 +50,89 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+      case Nil         => throw new RuntimeException("tail on empty list!")
+      case Cons(a, as) => as
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+      case Nil         => throw new RuntimeException("setHead on empty list!")
+      case Cons(a, as) => Cons(h, as)
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+      case Nil         => Nil
+      case _ if n == 0 => l
+      case Cons(a, as) => drop(as, n - 1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+      case Nil                 => Nil
+      case Cons(a, as) if f(a) => dropWhile(as, f)
+      case _                   => l
+    }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+      case Nil          => Nil
+      case Cons(a, Nil) => Nil
+      case Cons(a, as)  => Cons(a, init(as))
+   }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, b) => 1 + b)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+      case Nil         => z
+      case Cons(a, as) => foldLeft(as, f(z, a))(f)
+    }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def sumUsingFoldLeft(ns: List[Int]) = foldLeft(ns, 0)(_ + _)
+  def productUsingFoldLeft(ns: List[Int]) = foldLeft(ns, 1)(_ * _)
+  def lengthUsingFoldLeft[A](ns: List[A]) = foldLeft(ns, 0)((length, _) => length + 1)
+
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A])((b, a) => Cons(a, b))
+
+  def appendUsingFoldRight[A](a1: List[A], a2: List[A]): List[A] = foldRight(a1, a2)(Cons(_, _))
+
+  def concatenate[A](l: List[List[A]]): List[A] = foldRight(l, Nil: List[A])(append(_, _))
+
+  def addOne(l: List[Int]): List[Int] = l match {
+      case Nil         => Nil
+      case Cons(i, is) => Cons(i + 1, addOne(is))
+    }
+
+  def doubleToString(l: List[Double]): List[String] = l match {
+      case Nil         => Nil
+      case Cons(d, ds) => Cons(d.toString(), doubleToString(ds))
+    }
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+      case Nil         => Nil
+      case Cons(a, as) => Cons(f(a), map(as)(f))
+    }
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = l match {
+      case Nil                  => Nil
+      case Cons(a, as) if !f(a) => filter(as)(f)
+      case Cons(a, as)          => Cons(a, filter(as)(f))
+    }
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
+      case Nil         => Nil
+      case Cons(a, as) => append(f(a), flatMap(as)(f))
+    }
+
+  def filterUsingFlatmap[A](l: List[A])(f: A => Boolean): List[A] = flatMap(l)(a => if (f(a)) List(a) else Nil)
+
+  def zip(l1: List[Int], l2: List[Int]): List[Int] = (l1, l2) match {
+      case (_, Nil)                   => Nil
+      case (Nil, _)                   => Nil
+      case (Cons(a, as), Cons(b, bs)) => Cons(a + b, zip(as, bs))
+    }
+
+  def zipWith[A,B,C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = (l1, l2) match {
+      case (_, Nil)                   => Nil
+      case (Nil, _)                   => Nil
+      case (Cons(a, as), Cons(b, bs)) => Cons(f(a,b), zipWith(as, bs)(f))
+    }
+
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -4,10 +4,25 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-
 object Tree {
 
+  def size[A](t: Tree[A]): Int = t match {
+      case Leaf(_)             => 1
+      case Branch(left, right) => 1 + size(left) + size(right)
+    }
 
+  def maximum(t: Tree[Int]): Int = t match {
+      case Leaf(v)             => v
+      case Branch(left, right) => maximum(left).max(maximum(right))
+    }
 
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+      case Leaf(v)             => Leaf(f(v))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+    }
 
+  def fold[A,B](t: Tree[A])(l: A => B)(b: (B,B) => B): B = t match {
+      case Leaf(v)             => l(v)
+      case Branch(left, right) => b(fold(left)(l)(b), fold(right)(l)(b))
+    }
 }
